165dec4e028eb62ef99a04cfa78c721c7ad2a7e1
JaydenMontalvo <Jtmontalvo05@gmail.com>
2026-01-29 20:21:17 -0500

    Finished Application

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,4 +1,30 @@
 package org.example;
 
-public class Car {
+public class Car extends Vehicle {
+    private String brand;
+
+    public Car(int numberOfWheels, String color, float engineSize, String fuelType, String brand) {
+        super(numberOfWheels, color, engineSize, fuelType);
+        this.brand = brand;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void honk(){
+        System.out.println("Honk Honk");
+    }
+
+    public void displayInfo() {
+        System.out.println("Number of wheels: " + getNumberOfWheels());
+        System.out.println("Color: " + getColor());
+        System.out.println("Engine size: " + getEngineSize());
+        System.out.println("Fuel type: " + getFuelType());
+        System.out.println("Brand: " + getBrand());
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,31 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Vehicle vehicle1 = new Vehicle(4, "Red", 1.6f, "Gasoline");
+        Car car1 = new Car(6, "White", 300.2f, "Electric", "Tesla");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    //Vehicle method tests
+        System.out.println("--- VEHICLE1 ---");
+        System.out.println("Number of wheels: " + vehicle1.getNumberOfWheels());
+        System.out.println("Color: " + vehicle1.getColor());
+        System.out.println("Engine Size: " + vehicle1.getEngineSize());
+        System.out.println("Fuel Type: " + vehicle1.getFuelType());
+
+        //Car method test
+        System.out.println("--- CAR1 ---");
+        System.out.println("Number of wheels: " + car1.getNumberOfWheels());
+        System.out.println("Color: " + car1.getColor());
+        System.out.println("Engine Size: " + car1.getEngineSize());
+        System.out.println("Fuel Type: " + car1.getFuelType());
+        System.out.println("Brand Type: " + car1.getBrand());
+
+        //Honk (car method)
+        System.out.println("--- HONK METHOD ---");
+        car1.honk();
+
+        //displayInfo (car method)
+        System.out.println("--- DISPLAY INFO METHOD ---");
+        car1.displayInfo();
     }
 }
